8b83b61b03d95ab78d8be214db0414b85562b3c8
Felipe Noronha <felipenoris@gmail.com>
2019-08-30 00:33:48 -0300

    daily_forward_rates_vector

--- src/BinomialDaily.jl
+++ src/BinomialDaily.jl
@@ -10,6 +10,39 @@ com base nas taxas forward da curva.
 """
 module BinomialDaily
 
+import BusinessDays, InterestRates
+using Dates
+
+struct AmericanCall
+    riskfree_curve::InterestRates.IRCurve
+    dividend_yield::Float64
+    s0::Float64
+    k::Float64
+    σ::Float64
+    pricing_date::Date
+    maturity::Date
+end
+
+abstract type AbstractBinomialTree end
+
+mutable struct TreeNode{T<:AbstractBinomialTree}
+    current_step::Int
+    node_number::Int
+    s::Float64
+    price::Float64
+    tree::T
+end
+
+struct BinomialTree <: AbstractBinomialTree
+    contract::AmericanCall
+    days_to_maturity::Int
+    u::Float64
+    d::Float64
+    forward_rates::Vector{Float64}
+    risk_neutral_probabilities::Vector{Float64}
+    tree::Vector{Vector{TreeNode}}
+end
+
 """
     volatility_match(σ, Δt)
 
@@ -18,11 +51,63 @@ baseado no procedimento de Volatility Match
 proposto por Cox, Ross, Rubinstein (1979).
 """
 function volatility_match(σ, Δt) :: Tuple{Float64, Float64}
-    @assert σ ≧ 0 && Δt > 0
+    @assert σ >= 0 && Δt > 0
 
     u = exp(σ*sqrt(Δt))
     d = 1/u
     return u, d
+end
+
+function discountfactorforward(curve::InterestRates.AbstractIRCurve, d0::Date, d1::Date)
+    @assert d0 <= d1
+    return InterestRates.discountfactor(curve, d1) / InterestRates.discountfactor(curve, d0)
+end
+
+function daily_forward_rates_vector(curve::InterestRates.AbstractIRCurve, dividend_yield::Float64, pricing_date::Date, maturity::Date)
+    @assert maturity >= pricing_date
+    @assert InterestRates.curve_get_date(curve) == pricing_date
+    CAL = BusinessDays.BRSettlement()
+    @assert curve.daycount == InterestRates.BDays252(CAL) "Unsupported daycount: $(contract.daycount)."
+
+    result = Vector{Float64}()
+    d0 = pricing_date
+    Δt = 1 # fixo
+    d1 = BusinessDays.advancebdays(CAL, d0, Δt)
+
+    while d1 <= maturity
+        df = discountfactorforward(curve, d0, d1)
+        rate_continuous = -log(df) / Δt
+        push!(result, rate_continuous - dividend_yield)
+
+        # update d0, d1
+        d0 = d1
+        d1 = BusinessDays.advancebdays(CAL, d1, Δt)
+    end
+
+    return result
+end
+
+function risk_neutral_probabilities(rates::Vector{Float64}, u::Real, d::Real, Δt::Real)
+    return [ (exp(r*Δt) - d) / (u - d) for r in rates ]
+end
+
+function BinomialTree(contract::AmericanCall)
+    Δt = 1.0 # fixo
+    @assert contract.riskfree_curve.daycount == InterestRates.BDays252(BusinessDays.BRSettlement()) "Unsupported daycount: $(contract.daycount)."
+    dtm = BusinessDays.bdayscount(BusinessDays.BRSettlement(), contract.pricing_date, contract.maturity)
+
+    u, d = volatility_match(contract.σ, Δt)
+    rates = daily_forward_rates_vector(contract.riskfree_curve, contract.dividend_yield, contract.pricing_date, contract.maturity)
+
+    return BinomialTree(
+        contract,
+        dtm,
+        u,
+        d,
+        rates,
+        risk_neutral_probabilities(rates, u, d, Δt),
+        Vector{Vector{TreeNode}}()
+    )
 end
 
 end # module
